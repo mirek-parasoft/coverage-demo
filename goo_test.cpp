5f93c09c880a2efd4fa4b2181e05cd083ae39bd6
Mirek Zielinski <mirek@parasoft.com>
2022-04-12 09:21:46 -0400

    Testing improved

--- goo_test.cpp
+++ goo_test.cpp
@@ -10,3 +10,8 @@ TEST(GooTest, TestGetValueWithPositive) {
 TEST(GooTest, TestGetValueWithMixed_1) {
    EXPECT_EQ(getValue(0, 4), 0);
 }
+
+// 
+TEST(GooTest, TestGetValueWithZero) {
+   EXPECT_EQ(getValue(0, 0), 0);
+}
